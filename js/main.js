c612f698dbf49dd3db4f9922a74daa26174493b8
hmhealey <harrisonmhealey@gmail.com>
2015-03-10 16:58:34 -0400

    Added texture coordinates to meshes. Textures still aren't used though.

--- js/main.js
+++ js/main.js
@@ -31,15 +31,23 @@ var update = function(e) {
         "#version 100\n" +
         "\n" +
         "attribute vec3 position;\n" +
+        "attribute vec2 texCoord;\n" +
+        "\n" +
+        "varying vec2 fTexCoord;\n" +
         "\n" +
         "void main() {\n" +
         "    gl_Position = vec4(position, 1.0);\n" +
+        "    fTexCoord = texCoord;\n" +
         "}\n");
     shader.addShader(gl.FRAGMENT_SHADER,
         "#version 100\n" +
         "\n" +
+        "precision mediump float;\n" +
+        "\n" +
+        "varying vec2 fTexCoord;\n" +
+        "\n" +
         "void main() {\n" +
-        "    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);\n" +
+        "    gl_FragColor = vec4(0.0, fTexCoord, 1.0);\n" +
         "}\n");
     shader.initialize();
 
@@ -50,6 +58,12 @@ var update = function(e) {
         0, 0.5, 0,
         -0.5, -0.5, 0,
         0.5, -0.5, 0
+    ]);
+
+    mesh.setTexCoords([
+        0.5, 1.0,
+        0.0, 0.0,
+        1.0, 0.0
     ]);
 
     mesh.draw(shader);
--- js/mesh.js
+++ js/mesh.js
@@ -4,11 +4,13 @@ function Mesh(gl) {
     this.gl = gl;
 
     this.vertexBuffer = null;
+    this.texCoordBuffer = null;
     this.numVertices = 0;
 };
 
 Mesh.prototype.initialize = function() {
     if (!this.vertexBuffer) this.vertexBuffer = this.gl.createBuffer();
+    if (!this.texCoordBuffer) this.texCoordBuffer = this.gl.createBuffer();
 
     return this;
 };
@@ -18,28 +20,45 @@ Mesh.prototype.cleanup = function() {
         this.gl.deleteBuffer(this.vertexBuffer);
         this.vertexBuffer = null;
     }
+
+    if (this.texCoordBuffer) {
+        this.gl.deleteBuffer(this.texCoordBuffer);
+        this.texCoordBuffer = null;
+    }
 };
 
 Mesh.prototype.draw = function(shader) {
     this.gl.useProgram(shader.program);
 
-    this.gl.enableVertexAttribArray(0);
+    var positionLocation = this.gl.getAttribLocation(shader.program, "position");
+    this.gl.enableVertexAttribArray(positionLocation);
     this.gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-    this.gl.vertexAttribPointer(0, this.numVertices, gl.FLOAT, false, 0, 0);
+    this.gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+    var texCoordLocation = this.gl.getAttribLocation(shader.program, "texCoord");
+    this.gl.enableVertexAttribArray(texCoordLocation);
+    this.gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+    this.gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
 
     this.gl.drawArrays(gl.TRIANGLES, 0, this.numVertices);
 
     this.gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    this.gl.disableVertexAttribArray(0);
+    this.gl.disableVertexAttribArray(texCoordLocation);
+    this.gl.disableVertexAttribArray(positionLocation);
 
     this.gl.useProgram(null);
 };
 
 Mesh.prototype.setVertices = function(vertices) {
-    this.gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-    this.gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
     this.numVertices = vertices.length / 3;
 
+    this.gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+    this.gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    this.gl.bindBuffer(gl.ARRAY_BUFFER, null);
+};
+
+Mesh.prototype.setTexCoords = function(texCoords) {
+    this.gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+    this.gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
     this.gl.bindBuffer(gl.ARRAY_BUFFER, null);
 };
