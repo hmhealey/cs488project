9af8f9791d19ecae291c7ffe9da4681434811759
hmhealey <harrisonmhealey@gmail.com>
2015-03-15 13:18:21 -0400

    Fixed some assumptions that I was making about a lookAt matrix when constructing the matrices for a camera. I thought it translated the camera and that we'd have to invert it to get the view matrix, but it was actually the other way around.

--- js/camera.js
+++ js/camera.js
@@ -14,17 +14,15 @@ function Camera(args) {
     this.forward = args['forward'] || vec3.fromValues(0, 0, -1);
     this.up = args['up'] || vec3.fromValues(0, 1, 0);
 
-    this.matrix = null;
-    this.view = null;
-    this.projection = null;
+    this.matrix = mat4.create();
+    this.view = mat4.create();
+    this.projection = mat4.create();
     this.updateMatrices();
 };
 
 Camera.prototype.updateMatrices = function() {
-    this.matrix = mat4.lookAt(mat4.create(), this.position,
-                              vec3.add(vec3.create(), this.position, this.forward), this.up);
-    this.view = mat4.invert(mat4.create(), this.matrix);
+    mat4.lookAt(this.view, this.position, vec3.add(vec3.create(), this.position, this.forward), this.up);
+    mat4.invert(this.matrix, this.view);
 
-    this.projection = mat4.create();
     mat4.perspective(this.projection, this.fov * Math.PI / 180, this.aspect, this.near, this.far);
 };
